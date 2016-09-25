061653af2a1bc25e0efe17bdef8cf3274250df55
tesla <nikola@nikola.tesla>
2016-09-26 00:04:44 +0600

    Update some things

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render, HttpResponse
 from .forms import EmailSubmit
+from django.conf import settings
+from django.core.mail import send_mail
+
+
 
 # Create your views here.
 def index(request):
@@ -12,6 +16,16 @@ def formpage(request):
 
 def emailsubmit(request):
 	form_data = EmailSubmit(request.POST)
-	print(form_data)
-	saved_data = form_data.save()
-	return HttpResponse(form_data)
+	
+	saved_data = form_data.save(commit=False)
+	e1 = saved_data.email_one
+	e2 = saved_data.email_two
+
+	email_sub = 'Twin study form.'
+	email_mgs = 'Thank you for sign up. Please click this google form. https://docs.google.com/forms/d/e/1FAIpQLSfeFYikE4_LmlYG9kNF7LW4XoKdpT18euHzfqqUtPYCUdwXrA/viewform'
+
+	sent = send_mail(email_sub, email_mgs, settings.EMAIL_HOST_USER,
+         [e1, e2], fail_silently=False)
+	print("Email successfully sent to you and your twin. Please go to your mail address.")
+	saved_data.save()
+	return HttpResponse(saved_data)
--- twinapp/settings.py
+++ twinapp/settings.py
@@ -146,3 +146,13 @@ try:
     from .local_settings import *
 except ImportError:
     pass
+
+
+
+EMAIL_USE_TLS = True
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_PASSWORD = 'asddsa123' #my gmail password
+EMAIL_HOST_USER = 'megamind773@gmail.com' #my gmail username
+EMAIL_PORT = 587
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
